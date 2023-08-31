3683ccb45875da6a44c6822e305bc5d3b60b252b
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-31 16:49:31 +0630

    redo insertion_sort

--- insertion_sort/insertion_sort.js
+++ insertion_sort/insertion_sort.js
@@ -1,15 +1,16 @@
 function insertionSort(arr) {
-    for(let i = 1 ; i < arr.length; i++) {
-        let number_to_insert = arr[i]
-        let sorted_index = i-1
-        while(sorted_index >= 0 && number_to_insert < arr[sorted_index]) {
-            arr[sorted_index + 1] = arr[sorted_index]
-            sorted_index = sorted_index - 1
-        }
-        arr[sorted_index + 1] = number_to_insert
+  for(let i = 1; i < arr.length; i++) {
+    let unsortedEl = arr[i]
+    let sortedIndex = i - 1 
+    while(sortedIndex >= 0 && unsortedEl < arr[sortedIndex]) {
+        arr[sortedIndex + 1] = arr[sortedIndex]
+        sortedIndex = sortedIndex - 1 
     }
-    return arr;
+    arr[sortedIndex + 1] = unsortedEl
 }
+  return arr
+}
+// Big O notation O(n^2)
 
 const arr=[8,20,-2,4,-6]
 insertionSort(arr)
