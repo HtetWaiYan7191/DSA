c4621b9dddb896011e4149cce8856c26cd323af2
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-04 12:36:41 +0630

    Add big o in merge sort

--- merge_srot/merge_sort.js
+++ merge_srot/merge_sort.js
@@ -21,6 +21,7 @@ function merge(leftArr, rightArr) {
     }
     return [...sortedArr, ...leftArr, ...rightArr]
 }
+// Big O notation o(nlogn)
 
 const arr=[8,20,-2,4,-6]
 console.log(mergeSort(arr))
